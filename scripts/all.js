fc876e3cdc5c667e64cb6d937d50cfcd6c50c507
Keon <h21816595@gmail.com>
2022-12-14 13:27:02 +0800

    slick.js 下載檔案並修改scss/HTML主題修改

--- scripts/all.js
+++ scripts/all.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $(".slider").slick({
     infinite: false,
     speed: 300,
+    
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
@@ -10,13 +11,14 @@ $(document).ready(function () {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
+          
         }
       },
       {
         breakpoint: 520,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 3
+          slidesToScroll: 1
         }
       }
       // You can unslick at a given breakpoint now by adding:
